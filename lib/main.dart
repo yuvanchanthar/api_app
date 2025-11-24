0832a53024bbf701c56dc38b8e1674720bb1a04e
yuvanchanthar <yuvanpraveen182@gmail.com>
2025-11-24 14:48:54 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:api_integration_application/company_provider.dart';
 import 'package:api_integration_application/screen/company_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +12,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return ChangeNotifierProvider(
+      create: (context)=> CompanyProvider(),
+     child: MaterialApp(
       debugShowCheckedModeBanner: false,
       home: CompanyScreen()
-    );
+    ));
   }
 }
--- lib/screen/company_screen.dart
+++ lib/screen/company_screen.dart
@@ -1,7 +1,8 @@
+import 'package:api_integration_application/company_provider.dart';
 import 'package:api_integration_application/model/company.dart';
 import 'package:api_integration_application/service/company_service.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 import 'create_company.dart';
 
 class CompanyScreen extends StatefulWidget {
@@ -12,6 +13,14 @@ class CompanyScreen extends StatefulWidget {
 }
 
 class _CompanyScreenState extends State<CompanyScreen> {
+  @override void initState() {
+    
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      
+    },);
+    Provider.of<CompanyProvider>(context, listen: false).getAllCompany();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,74 +42,78 @@ class _CompanyScreenState extends State<CompanyScreen> {
         child: const Icon(Icons.add),
       ),
 
-      body: FutureBuilder(
-        future: CompanyService().getAllCompany(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text("Error loading data"));
-          }
-
-          if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          final List<Company> companies = snapshot.data!;
-
-          return ListView.builder(
-            itemCount: companies.length,
-            itemBuilder: (context, index) {
-              final company = companies[index];
-
-              return Card(
-                elevation: 2,
-                margin: const EdgeInsets.all(8),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      company.logo ??
-                          "https://logo.clearbit.com/google.com",
+      body: Consumer<CompanyProvider>(builder: (context, value, child) {
+        return 
+      
+        FutureBuilder(
+          future: CompanyService().getAllCompany(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Center(child: Text("Error loading data"));
+            }
+        
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+        
+            final List<Company> companies = snapshot.data!;
+        
+            return ListView.builder(
+              itemCount: companies.length,
+              itemBuilder: (context, index) {
+                final company = companies[index];
+        
+                return Card(
+                  elevation: 2,
+                  margin: const EdgeInsets.all(8),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        company.logo ??
+                            "https://logo.clearbit.com/google.com",
+                      ),
+                    ),
+                    title: Text(company.name ?? "No name"),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(company.phone ?? "No phone"),
+                        Text(company.address ?? "No address"),
+                      ],
+                    ),
+        
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        
+                        IconButton(
+                          icon: const Icon(Icons.edit, color: Colors.blue),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => CreateCompany(company: company),
+                              ),
+                            ).then((_) => setState(() {}));
+                          },
+                        ),
+        
+                        
+                        IconButton(
+                          icon: const Icon(Icons.delete, color: Colors.red),
+                          onPressed: () {
+                            _confirmDelete(company.id!);
+                          },
+                        ),
+                      ],
                     ),
                   ),
-                  title: Text(company.name ?? "No name"),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(company.phone ?? "No phone"),
-                      Text(company.address ?? "No address"),
-                    ],
-                  ),
-
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      
-                      IconButton(
-                        icon: const Icon(Icons.edit, color: Colors.blue),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => CreateCompany(company: company),
-                            ),
-                          ).then((_) => setState(() {}));
-                        },
-                      ),
-
-                      
-                      IconButton(
-                        icon: const Icon(Icons.delete, color: Colors.red),
-                        onPressed: () {
-                          _confirmDelete(company.id!);
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
-        },
-      ),
+                );
+              },
+            );
+          },
+        );
+  }),
     );
   }
 
@@ -128,3 +141,4 @@ class _CompanyScreenState extends State<CompanyScreen> {
     );
   }
 }
+
